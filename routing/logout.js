458d58224b0be0e9a7844e657888c6da73bfb064
sebaswit34 <sebastian.swit.2003@o2.pl>
2025-04-02 01:30:33 +0200

    Update logout.js

--- routing/logout.js
+++ routing/logout.js
@@ -1,16 +1,9 @@
-const logoutRouting = (method, response) => {
-  response.setHeader("Content-Type", "text/html");
-  response.write("<html>");
-  response.write("<head><title>Shop - Logout</title></head>");
-  response.write("<body>");
-  response.write("<h1>Logout</h1>");
-  response.write(
-    "<nav><a href='/'>Home</a><br /><a href='/kill'>Logout from application</a></nav>"
-  );
-  response.write("</body>");
-  response.write("</html>");
+const express = require("express");
+const path = require("path");
 
-  return response.end();
-};
+const router = express.Router();
 
-module.exports = { logoutRouting };
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../views", "logout.html"));
+});
+module.exports = router;
