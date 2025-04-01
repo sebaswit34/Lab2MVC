efcfa3b4be6c8bb0c1165546c7c0d328fcdb032e
sebaswit34 <sebastian.swit.2003@o2.pl>
2025-04-02 01:35:45 +0200

    Update renderNewProductPage.js

--- views/renderNewProductPage.js
+++ views/renderNewProductPage.js
@@ -1,1 +1,23 @@
+const renderNewProductPage = (response) => {
+    fileSystem.readFile("./product.txt", "utf-8", (err, data) => {
+        response.setHeader("Content-Type", "text/html");
+        response.write("<html>");
+        response.write("<head><title>Shop - Newest product</title></head>");
+        response.write("<body>");
+        response.write("<h1>Newest product</h1>");
+        response.write(
+            "<nav><a href='/'>Home</a><br /><a href='/product/add'>Add product</a><br /><a href='/logout'>Logout</a></nav>"
+        );
 
+        if (err) {
+            response.write("<br /><div>No new products available.</div>");
+        } else {
+            response.write(`<br /><div>New product data - ${splittedData}</div>`);
+        }
+
+        response.write("</body>");
+        response.write("</html>");
+
+        return response.end();
+    });
+};
