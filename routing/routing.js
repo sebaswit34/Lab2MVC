9f6eaf51f0a7ee84b3831e824e055e87b86755b9
sebaswit34 <sebastian.swit.2003@o2.pl>
2025-04-02 01:32:34 +0200

    Update routing.js

--- routing/routing.js
+++ routing/routing.js
@@ -4,42 +4,32 @@ const { logoutRouting } = require("./logout");
 const { STATUS_CODE } = require("../constants/statusCode");
 
 const requestRouting = (request, response) => {
-  const { url, method } = request;
-  console.log(
-    `INFO (${new Date(Date.now()).toUTCString()}): ${method} - ${url}`
-  );
+    const { url, method } = request;
+   getInfoLog(method, url);
 
-  if (url === "/") {
-    return homeRouting(method, response);
-  }
+    if (url === "/") {
+        return homeRouting(method, response);
+    }
 
-  if (url.includes("/product")) {
-    return productRouting(request, response);
-  }
+    if (url.includes("/product")) {
+        return productRouting(request, response);
+    }
 
-  if (url === "/logout") {
-    return logoutRouting(method, response);
-  }
+    if (url === "/logout") {
+        return logoutRouting(method, response);
+    }
 
-  if (url === "/kill") {
-    console.log(
-      `PROCESS (${new Date(
-        Date.now()
-      ).toUTCString()}): logout has been initiated and the application will be closed.`
-    );
-    process.exit();
-  }
+    if (url === "/kill") {
+        return getProccessLog("logout has been initiated and the application will be closed.")
+        process.exit();
+    }
 
-  response.statusCode = STATUS_CODE.NOT_FOUND;
-  response.setHeader("Content-Type", "text/html");
-  response.write("<html><body><h1>404 - Not Found</h1></body></html>");
-  response.end();
+    response.statusCode = STATUS_CODE.NOT_FOUND;
+    response.setHeader("Content-Type", "text/html");
+    response.write("<html><body><h1>404 - Not Found</h1></body></html>");
+    response.end();
 
-  console.warn(
-    `ERROR (${new Date(
-      Date.now()
-    ).toUTCString()}): requested url ${url} doesn't exist.`
-  );
+    getErrorLog(url);
 };
 
 module.exports = { requestRouting };
