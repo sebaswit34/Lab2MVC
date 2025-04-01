c0135735c76248acc6c4f70468d4254bd76e6677
sebaswit34 <sebastian.swit.2003@o2.pl>
2025-04-02 01:36:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,40 +1,34 @@
-/*
-  📦 Dependy the Importer  
-  Zaimportuj wszystkie wymagane moduły: path, express, body-parser, logger oraz routing.  
-*/
-const http = require("http");
+const {PORT} = require("./config");
 const config = require("./config");
-const { requestRouting } = require("./routing/routing");
+const logger = require("./utils/logger");
+const productRoutes = require("./routing/product");
+const logoutRoutes = require("./routing/logout");
+const killRoutes = require("./routing/kill");
+const homeRoutes = require("./routing/home");
+const { STATUS_CODE } = require("./constants/statusCode");
 
-const requestListener = (request, response) => {
-  requestRouting(request, response);
-};
+const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const server = http.createServer(requestListener);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
-server.listen(config.PORT);
+app.use((req, res, next) => {
+   logger.getInfoLog(req.method, req.url);
+   next();
+});
 
-/*
-  🏗 Structo the Builder  
-  Utwórz instancję aplikacji express i zapisz ją w stałej app.  
-*/
-/*
-  🏗 Structo the Builder  
-  Zarejestruj middleware body-parser do parsowania ciał formularzy. 
-*/
-/*
-  🏗 Structo the Builder  
-  Dodaj middleware logujący informacje o każdym przychodzącym żądaniu.  
-*/
-/*
-  🏗 Structo the Builder  
-  Zarejestruj middleware obsługujące poszczególne ścieżki.  
-*/
-/*
-  🏗 Structo the Builder  
-    Obsłuż stronę 404 – zwróć plik 404.html i zaloguj błąd.   
-*/
-/*
-  🏗 Structo the Builder  
-    Uruchom serwer i nasłuchuj na porcie z config.js.    
-*/
+app.use("/product", productRoutes);
+app.use("/logout", logoutRoutes);
+app.use("/kill", killRoutes);
+app.use("/home", homeRoutes);
+
+app.use((req, res) => {
+   logger.getErrorLog(req.url);
+   res.status(STATUS_CODE.NOT_FOUND).sendFile(path.join(__dirname, "..", "views", "404.html"));
+});
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
+});
